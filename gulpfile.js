836941b4ef53c3e63232e529ecb42d633d08e01e
Geovanny Ramirez <giorc93@hotmail.com>
2021-03-16 23:31:21 -0500

    Gulp default rm #9

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp = require('gulp-v3');
+const gulp = require('gulp');
 const gap = require("gulp-append-prepend");
 
 gulp.task("licenses", async function () {
@@ -72,5 +72,3 @@ gulp.task("licenses", async function () {
     .pipe(gulp.dest("./", { overwrite: true }));
   return;
 });
-
-gulp.task("default", ['licenses']);
